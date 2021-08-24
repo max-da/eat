6de92fa46eb7c4e3678a967d6c7bd3540ae72528
micaelagoffe <micaela.goffe@gmail.com>
2021-08-24 13:21:12 +0200

    setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Navbar } from './components/Navbar';
+import { Startpage } from './pages/Startpage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/"><Startpage /></Route>
+        </Switch>
+      </Router>
   );
 }
 
---
+++ client/src/pages/Startpage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Startpage = () => {
+
+    return (
+        <>Startpage</>
+    )
+}
