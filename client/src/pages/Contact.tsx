10ba70f1a775949e17910a3e4fbb4409ed9c334c
feliciatranberg <felicia@substansgbg.se>
2021-08-26 00:18:42 +0200

    some styling

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPoo, faGlassMartiniAlt, faPeopleArrows } from '@fortawesome/free-solid-svg-icons'
 import { PageWrapper, TextDiv, H1, H2, H3, H4Light } from "./styles/GlobalStylesPages";
 
 const Li = styled.li`
@@ -6,6 +8,12 @@ const Li = styled.li`
         text-align: center;
         text-decoration: underline;
         cursor: pointer;`
+
+        const Icons = {
+          color: "darkgrey",
+          fontSize: "70px",
+          margin: "5%"
+        }
 
 export const Contact = () => {
 
@@ -18,6 +26,9 @@ export const Contact = () => {
       <H4Light> Tisdag - Torsdag 17:00-23:00 </H4Light>
       <H4Light> Fredag - Lördag 17:00-00:30 </H4Light>
       <H4Light> Söndagar 17:00-21:00</H4Light>
+      <FontAwesomeIcon style={Icons} icon={faGlassMartiniAlt} />
+      <FontAwesomeIcon style={Icons} icon={faPeopleArrows} />
+      <FontAwesomeIcon style={Icons} icon={faPoo} />
       </TextDiv>
     </PageWrapper> 
     );
