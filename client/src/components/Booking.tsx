01acd65cea23c0683b4fff9757d3d861e5a67b97
max-da <max.dahlbom@outlook.com>
2021-08-26 09:21:40 +0200

    merge

--- client/src/components/Booking.tsx
+++ client/src/components/Booking.tsx
@@ -1,4 +1,4 @@
-import { useState ,ChangeEvent} from "react"
+import { useState ,ChangeEvent, useEffect} from "react"
 import axios from "axios";
 import { TableComponent } from "./TableComponent";
 
